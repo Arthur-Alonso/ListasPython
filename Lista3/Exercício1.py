54d0d6a0100b3fdef023d4a695e49163af859a9a
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 10:54:13 -0300

    Create Exercício1.py

---
+++ Lista3/Exercício1.py
@@ -0,0 +1,8 @@
+"""
+1 - Escreva um programa que peça um número inteiro positivo ao usuário e exiba todos os
+números de 1 até esse número, um por linha.
+"""
+
+x = int(input("Insira um número inteiro: "))
+for a in range(x):
+    print(a+1)
