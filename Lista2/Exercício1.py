6fc883036e0d69baf0159b920de9fd2c26ac23e5
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 10:40:52 -0300

    Create Exercício1.py

---
+++ Lista2/Exercício1.py
@@ -0,0 +1,9 @@
+"""
+1 - Escreva um programa que solicite um número inteiro ao usuário e informe se ele é par ou ímpar.
+"""
+
+num = int(input("Insira um número: "))
+if num % 2 == 0:
+    print("Par")
+else:
+    print("Impar")
