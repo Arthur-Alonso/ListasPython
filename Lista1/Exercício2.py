538a3d19c99742d0ed3b435adf6a3ac05b80e366
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 10:26:00 -0300

    Create Exercício2.py

---
+++ Lista1/Exercício2.py
@@ -0,0 +1,8 @@
+"""
+2 - Faça um programa que leia uma temperatura em graus Celsius e a converta para Fahrenheit.
+    Fórmula: F = (C X 9/5) + 32
+"""
+
+celsius = float(input("Informe a temperatura em Celsius: "))
+fahrenheit = 9/5 * celsius + 32
+print(fahrenheit)
