69b999ceef590000dada6208c8742cc44fa57135
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 10:38:40 -0300

    Create Exercício5.py

---
+++ Lista1/Exercício5.py
@@ -0,0 +1,8 @@
+"""
+5 - Desenvolva um programa que receba um valor em metros e o converta para centímetros.
+    Sabemos que: 1 metro = 100 centímetros
+"""
+
+metro = float(input("Digite o valor em metros"))
+centimetros = metro*100
+print(centimetros)
