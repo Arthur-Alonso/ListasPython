fb1831e8aac3efa5f4c45040eeb77cde19e7490d
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 11:09:10 -0300

    Update Exercício1.py

--- Lista4/Exercício1.py
+++ Lista4/Exercício1.py
@@ -1,1 +1,8 @@
+"""
+1 - Escreva um programa que solicite ao usuário uma lista de números inteiros e
+calcule a soma de todos os elementos da lista.
+"""
 
+numeros = list(map(int, input("Digite uma lista de números inteiros separados por espaço: ").split()))
+soma = sum(numeros)
+print("A soma dos elementos da lista é:", soma)
