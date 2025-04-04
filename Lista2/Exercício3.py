8c5ee974f6ec089a09c7f3d338f4865752e812df
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 10:46:38 -0300

    Update Exercício3.py

--- Lista2/Exercício3.py
+++ Lista2/Exercício3.py
@@ -1,5 +1,5 @@
 """
-Crie um programa que peça a idade de uma pessoa e informe sua classificação:
+3 - Crie um programa que peça a idade de uma pessoa e informe sua classificação:
 • Menor de idade (menos de 18 anos)
 • Adulto (entre 18 e 59 anos)
 • Idoso (60 anos ou mais)
