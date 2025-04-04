48a3f19b448e88a1683f356cc6e7510d813aca04
Arthur-Alonso <arthuralonso1@hotmail.com>
2025-04-04 10:28:38 -0300

    Update Exercício1.py

--- Lista1/Exercício1.py
+++ Lista1/Exercício1.py
@@ -1,6 +1,6 @@
 """
 1 - Escreva um programa que peça ao usuário o valor do raio de um círculo e calcule sua área.
-    Use a fórmula: Area = PI* raio
+    Fórmula: Area = PI* raio
 """
 
 import math
